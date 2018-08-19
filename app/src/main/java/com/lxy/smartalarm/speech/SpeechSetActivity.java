64e6ab79e9a8f976629f435059e7575b51a8c90c
lxy8250 <1445925233@qq.com>
2018-08-19 09:50:27 +0800

    添加闹钟提醒界面，和语音播报

---
+++ app/src/main/java/com/lxy/smartalarm/speech/SpeechSetActivity.java
@@ -0,0 +1,15 @@
+package com.lxy.smartalarm.speech;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lxy.smartalarm.R;
+
+public class SpeechSetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speech_set);
+    }
+}
